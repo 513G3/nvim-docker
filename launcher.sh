bc485e7705d4ddfd8d21d78f1c739a28d3c1d2a3
513G3 <29024145+513G3@users.noreply.github.com>
2024-01-19 10:52:18 -0700

    More justification of long startup delay

--- launcher.sh
+++ launcher.sh
@@ -1,15 +1,16 @@
 #!/bin/bash
 
-# Fix the uid/gid
-echo "This can take some time..."
-echo ""
-echo "... setting the uid/gid for the docker user in the container"
-echo "so that filesystem changes made in the container's bind mount"
-echo "will map back to ${USER}"
-fixuid -q 
-
 # Set $HOME
 export HOME=/home/docker
 
-# Run Neovim
+# Fix the UID:GID
+echo "This can take some time... recursively changing the"
+echo "UID:GID (and file ownerships) of the docker user in"
+echo "the container so that:"
+echo "*  You can execute things owned by the docker user"
+echo "*  Filesystem changes that you make in the"
+echo "   container's bind mount will map back to you"
+fixuid -q 
+
+# Run nvim
 ~/nvim-linux64/bin/nvim $1
