3f8ec12d3ccf5986bc80d9b8959a5646ecefde59
513G3 <29024145+513G3@users.noreply.github.com>
2024-01-18 21:05:18 -0700

    More tweaks to the unfortunate delay

--- launcher.sh
+++ launcher.sh
@@ -1,10 +1,12 @@
 #!/bin/bash
 
 # Fix the uid/gid
-echo "Now setting the uid/gid for the docker user in the container"
-echo "so that filesystem changes that you make in the container's"
-echo "bind mount will map to you on the host"
-fixuid -q
+echo "This can take some time..."
+echo ""
+echo "... setting the uid/gid for the docker user in the container"
+echo "so that filesystem changes made in the container's bind mount"
+echo "will map back to ${USER}"
+fixuid -q 
 
 # Set $HOME
 export HOME=/home/docker
