8bea8bdc35b56e7c5d254d90dffdeac3478feee1
513G3 <29024145+513G3@users.noreply.github.com>
2024-01-30 14:27:57 -0700

    Change working dir before launching nvim

--- launcher.sh
+++ launcher.sh
@@ -1,4 +1,13 @@
 #!/bin/bash
 
+# Change to the new work directory
+echo "$1"
+if [ -d "$1" ]; then
+  DIR="$1"
+else
+  DIR=$(dirname "$1")
+fi
+cd "$DIR" || return 1
+
 # Run nvim
 ~/nvim-linux64/bin/nvim "$1"
