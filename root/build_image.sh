bd6b2ecafaaed84993fe0b3132c721d9c10b752d
513G3 <29024145+513G3@users.noreply.github.com>
2024-02-24 13:57:51 -0700

    Bump Neovim config to 1.15

--- root/build_image.sh
+++ root/build_image.sh
@@ -4,5 +4,6 @@ TAG=nvim-docker-root
 
 echo "Building $TAG with UID:GID 0:0"
 cp ../launcher.sh .
-docker buildx build --tag $TAG --no-cache .
+#docker buildx build --tag $TAG --no-cache .
+docker buildx build --tag $TAG .
 rm launcher.sh
