a23b5440ddc0387ca2a09dae3c2a8fa7f4eaaef2
513G3 <29024145+513G3@users.noreply.github.com>
2024-03-08 04:10:03 +0000

    Bring in stuff for git and creds

--- user/build_image.sh
+++ user/build_image.sh
@@ -4,6 +4,13 @@ TAG=nvim-docker-$USER
 GID=$(id -g "$USER")
 
 echo "Building $TAG with UID:GID $UID:$GID"
-cp ../launcher.sh .
-docker buildx build --tag nvim-docker-"$USER" --build-arg _USER="$USER" --build-arg _UID="$UID" --build-arg _GID="$GID" --no-cache .
-rm launcher.sh
+
+# Provide git-needed stuff for fugitive
+cp -r ~/.ssh .
+cp ~/.gitconfig .
+
+# Build
+docker buildx build --no-cache --tag nvim-docker-"$USER" --build-arg _USER="$USER" --build-arg _UID="$UID" --build-arg _GID="$GID" .
+
+# Cleanup
+rm -fr .ssh .gitconfig
