979f8254c58a3951823d8462b81db2fa93d9acfd
513G3 <29024145+513G3@users.noreply.github.com>
2024-03-08 19:46:32 -0700

    Refine ssh and git files

--- user/build_image.sh
+++ user/build_image.sh
@@ -5,12 +5,16 @@ GID=$(id -g "$USER")
 
 echo "Building $TAG with UID:GID $UID:$GID"
 
-# Provide git-needed stuff for fugitive
-cp -r ~/.ssh .
-cp ~/.gitconfig .
+# Provide ssh and git stuff (best effort)
+if [ -d "$HOME/.ssh" ]; then
+    cp -r "$HOME/.ssh" .
+fi
+if [ -f "$HOME/.gitconfig" ]; then
+    cp "$HOME/.gitconfig" .
+fi
 
 # Build
 docker buildx build --no-cache --tag nvim-docker-"$USER" --build-arg _USER="$USER" --build-arg _UID="$UID" --build-arg _GID="$GID" .
 
 # Cleanup
-rm -fr .ssh .gitconfig
+rm -fr .ssh .gitconfig >> /dev/null 2>&1
